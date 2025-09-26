182f195872809a1fa3880ae90ddecce5e3cca016
Rudra092 <rudra.092@gmail.com>
2025-09-26 13:37:51 +0530

    sixth commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://reactlogin-hovt.onrender.com/api", // Replace with your backend
+  baseURL: "https://reactlogin-hovt.onrender.com/api/auth",
 });
 
 // Example requests
