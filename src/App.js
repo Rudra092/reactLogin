1ca3c595ecfff2c52a92e728038a8d90685e487b
Rudra092 <rudra.092@gmail.com>
2025-10-22 14:25:44 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import Signup from './components/signup';
 import ForgotPassword from './components/forgot-password';
+import ResetPassword from './components/ResetPassword';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup/>} />
         <Route path="/forgot-password" element={<ForgotPassword/>} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
       </Routes>
     </Router> 
     </div>
